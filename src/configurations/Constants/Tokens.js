ab3933e8843fd35bdfbc4fbf7b43a7d59353dfd1
chrlschwb <chrlschwb77@outlook.com>
2022-03-29 12:47:15 -0800

    init

---
+++ src/configurations/Constants/Tokens.js
@@ -0,0 +1,15 @@
+import { CHAIN_ID } from "."
+
+const { BSC_MAINNET, BSC_TESTNET } = CHAIN_ID
+
+export const bscTokens = {
+  SAFUTRENDZ: {
+    [BSC_MAINNET]: {
+      name: "SaFuTrendz",
+      symbol: "SAFU",
+      decimal: 18,
+      address: "0x4B3370B44A9E6A4EC28BfDb7C2C400758b59975d",
+      projectUrl: "https://www.safutrendz.com/",
+    },
+  },
+}
